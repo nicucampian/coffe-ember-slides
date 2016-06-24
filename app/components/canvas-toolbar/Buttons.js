cf424a48c5ac0953aaccfdb104b3750c7bdde354
Madalina Pintea <madalina.pintea@softvision.ro>
2016-06-24 17:00:51 +0300

    Add layouts feature & Structure updates
    - get layouts and all layout shapes
    - header components code updates and styling
    - slide list stucture updates and styling
    - canvas style update
    - tabs for layouts and properties

--- app/components/canvas-toolbar/Buttons.js
+++ app/components/canvas-toolbar/Buttons.js
@@ -2,21 +2,26 @@ import Ember from 'ember';
 
 export const buttons = [
   Ember.Object.create({
-    text: 'Add text',
+    text: 'Add line'
   }),
   Ember.Object.create({
-    text: 'Add image',
+    // Add text
+    content: Ember.String.htmlSafe('<i class="fa fa-text-height" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
-    text: 'Add line',
+    // Add image
+    content: Ember.String.htmlSafe('<i class="fa fa-picture-o" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
-    icon: 'assets/toolbar/left.png',
+    content: Ember.String.htmlSafe('<i class="fa fa-align-left" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
-    icon: 'assets/toolbar/center.png',
+    content: Ember.String.htmlSafe('<i class="fa fa-align-justify" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
-    icon: 'assets/toolbar/center.png',
+    content: Ember.String.htmlSafe('<i class="fa fa-align-center" aria-hidden="true"></i>')
+  }),
+  Ember.Object.create({
+    content: Ember.String.htmlSafe('<i class="fa fa-align-right" aria-hidden="true"></i>')
   }),
 ];
--- mirage/config.js
+++ mirage/config.js
@@ -2,11 +2,11 @@ const slides = [{ id: 1, title: 'Slide 1'},
             { id: 2, title: 'Slide 2', shapes: ['4'] },
             { id: 3, title: 'Slide 3', shapes: ['5'] }];
 
-const layoutObjects =
+const layoutObjects = 
     [
       {
           shapes: [0],
-          url: "assets/layouts/blank.png",
+          url: "assets/layouts/blank.png",    
           id: 0
       },
       {
@@ -15,7 +15,7 @@ const layoutObjects =
           id: 1
       },
       {
-          shapes: [2,3,4,5],
+          shapes: [2,3,4,5],             
           url: "assets/layouts/title-2content-image.png",
           id: 2
       },
@@ -56,175 +56,175 @@ const layoutObjects =
       }
   ];
 
-const shapes =
+const shapes = 
   [
-     {
+     {        
       type: "text",
       content: "Click to add Title",
       x: "30%",
       y: "50%",
-      width: "200px",
-      height: "50px",
+      width: "200",
+      height: "50",        
       id: 0,
       layoutId: 0
     },
-    {
+    { 
       type: "text",
       content: "Click to add Title",
       x: "30%",
       y: "50%",
-      width: "200px",
-      height: "50px",
+      width: "200",
+      height: "50",        
       id: 1,
       slide: 1
     },
-    {
+    {        
       type: "text",
       content: "Click to add Title",
       x: "30%",
       y: "10%",
-      width: "100px",
-      height: "30px",
+      width: "100",
+      height: "30",
       id: 2,
       slide: 1
     },
-    {
+    { 
       type: "text",
       content: "Click to add Text",
       x: "20%",
       y: "43%",
-      width: "100px",
-      height: "80px",
+      width: "100",
+      height: "80",        
       id: 3,
       slide: 2
     },
-    {
+    { 
       type: "text",
       content: "Click to add Text",
       x: "20%",
       y: "76%",
-      width: "100px",
-      height: "80px",
+      width: "100",
+      height: "80",     
       id: 4,
       slide: 2
     },
-    {
+    { 
       type: "picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "66%",
       y: "66%",
-      width: "200px",
-      height: "200px",
+      width: "200",
+      height: "200",   
       id: 5,
       slide: 3
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "20%",
       y: "66%",
-      width: "200px",
-      height: "200px",
+      width: "200",
+      height: "200",    
       id: 6,
       slide: 1
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "60%",
       y: "66%",
-      width: "200px",
-      height: "200px",
+      width: "200",
+      height: "200",    
       id: 7,
       slide: 1
     },
-    {
+    { 
       type: "picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "20%",
       y: "43%",
-      width: "200px",
-      height: "200px",
+      width: "200",
+      height: "200",
       id: 8,
       layoutId: 4
     },
-    {
+    { 
       type: "picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "40%",
       y: "43%",
-      width: "200px",
-      height: "200px",
+      width: "200",
+      height: "200",
       id: 9,
       layoutId: 4
     },
-    {
+    { 
       type: "picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "20%",
       y: "76%",
-      width: "300px",
-      height: "200px",
+      width: "300",
+      height: "200",
       id: 10,
       layoutId: 4
-    },
-    {
+    },      
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "20%",
       y: "43%",
-      width: "150px",
-      height: "150px",
+      width: "150",
+      height: "150",
       id: 11,
       slide: 3
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "60%",
       y: "76%",
-      width: "150px",
-      height: "150px",
+      width: "150",
+      height: "150",
       id: 12,
       layoutId: 5
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "20%",
       y: "76%",
-      width: "150px",
-      height: "150px",
+      width: "150",
+      height: "150",
       id: 13,
       layoutId: 5
-    },
-    {
+    },     
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "15%",
       y: "43%",
-      width: "100px",
-      height: "100px",
+      width: "100",
+      height: "100",
       id: 14,
       layoutId: 6
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "45%",
       y: "43%",
-      width: "100px",
-      height: "100px",
+      width: "100",
+      height: "100",
       id: 15,
       layoutId: 6
-    },
-    {
+    },      
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "15%",
       y: "76%",
-      width: "100px",
-      height: "100px",
+      width: "100",
+      height: "100",
       id: 16,
       layoutId: 6
     },
@@ -233,58 +233,58 @@ const shapes =
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "45%",
       y: "76%",
-      width: "100px",
-      height: "100px",
+      width: "100",
+      height: "100",
       id: 17,
       layoutId: 6
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "75%",
       y: "76%",
-      width: "100px",
-      height: "100px",
+      width: "100",
+      height: "100",
       id: 18,
       layoutId: 6
-    },
-    {
+    },     
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "35%",
       y: "43%",
-      width: "250px",
-      height: "250px",
+      width: "250",
+      height: "250",
       id: 19,
       layoutId: 7
-    },
-    {
+    },     
+    { 
       type:"text",
       content: "Click to add Text",
       x: "50%",
       y: "43%",
-      width: "200px",
-      height: "100px",
+      width: "200",
+      height: "100",
       id: 20,
       layoutId: 8
     },
-    {
+    { 
       type:"picture",
       content: "https://pbs.twimg.com/profile_images/528137831542038528/S79NgwqB.png",
       x: "15%",
       y: "73%",
-      width: "200px",
-      height: "200px",
+      width: "200",
+      height: "200",
       id: 21,
       layoutId: 8
-    },
-    {
+    },     
+    { 
       type:"text",
       content: "Click to add Text",
       x: "20%",
       y: "43%",
-      width: "500px",
-      height: "400px",
+      width: "500",
+      height: "400",
       id: 22,
       layoutId: 9
     }
@@ -302,6 +302,7 @@ export default function () {
     const id = request.queryParams.id;
     const type = request.queryParams.type;
     let data = null;
+
     if(type === 'slides') {
       data = shapes.filter(function(shape) {
         return shape.slide == id;
