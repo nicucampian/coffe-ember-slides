fc759a66c18041d1e72b24979da6ef017b04d469
Diana Farcau <diana.farcau@softvision.ro>
2016-06-29 14:54:50 +0300

    Clean the code and updated the styles

--- app/components/canvas-toolbar/Buttons.js
+++ app/components/canvas-toolbar/Buttons.js
@@ -5,23 +5,27 @@ export const buttons = [
     text: 'Add line'
   }),
   Ember.Object.create({
-    // Add text
+    // Add icon for text
     content: Ember.String.htmlSafe('<i class="fa fa-text-height" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
-    // Add image
+    // Add icon for picture
     content: Ember.String.htmlSafe('<i class="fa fa-picture-o" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
+    // Add icon for text - left align
     content: Ember.String.htmlSafe('<i class="fa fa-align-left" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
+    // Add icon for text - justify align
     content: Ember.String.htmlSafe('<i class="fa fa-align-justify" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
+    // Add icon for text - center align
     content: Ember.String.htmlSafe('<i class="fa fa-align-center" aria-hidden="true"></i>')
   }),
   Ember.Object.create({
+    // Add icon for text - right align
     content: Ember.String.htmlSafe('<i class="fa fa-align-right" aria-hidden="true"></i>')
   }),
 ];
